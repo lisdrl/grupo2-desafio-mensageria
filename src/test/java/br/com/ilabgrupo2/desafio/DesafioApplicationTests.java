d4431ca9d681ab3e46fddbea9b2f7fd390d303bf
lisdrl <lisandre.darioli@gmail.com>
2022-05-02 10:53:05 -0300

    First import

---
+++ src/test/java/br/com/ilabgrupo2/desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ilabgrupo2.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
