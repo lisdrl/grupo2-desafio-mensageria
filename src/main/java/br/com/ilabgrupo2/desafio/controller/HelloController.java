208ae4a282b46023f21f5cf34b357ea212b7f896
Biancaandradee <bi_andradehandan@hotmail.com>
2022-05-02 11:54:09 -0300

    upload

--- src/main/java/br/com/ilabgrupo2/desafio/controller/HelloController.java
+++ src/main/java/br/com/ilabgrupo2/desafio/controller/HelloController.java
@@ -1,8 +1,12 @@
 package br.com.ilabgrupo2.desafio.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class HelloController {
@@ -12,4 +16,29 @@ public class HelloController {
 
         return "home";
     }
+	
+     
+    @PostMapping("/upload")
+    public String handleUploadForm(Model model, String description,
+            @RequestParam("file") MultipartFile multipart) {
+        String fileName = multipart.getOriginalFilename();
+         
+        System.out.println("Description: " + description);
+        System.out.println("filename: " + fileName);
+         
+        String message = "";
+         
+        try {
+            UploadController.uploadFile(fileName, multipart.getInputStream());
+            message = "Your file has been uploaded successfully!";
+        } catch (Exception ex) {
+            message = "Error uploading file: " + ex.getMessage();
+        }
+         
+        model.addAttribute("message", message);
+         
+        return "message";              
+    }
 }
+
+
