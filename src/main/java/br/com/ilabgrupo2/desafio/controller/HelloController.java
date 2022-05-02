d3948fb3398e95da08750b3491f8526fa520ba8f
lisdrl <lisandre.darioli@gmail.com>
2022-05-02 13:08:17 -0300

    Updating HelloControler and S3Util

--- src/main/java/br/com/ilabgrupo2/desafio/controller/HelloController.java
+++ src/main/java/br/com/ilabgrupo2/desafio/controller/HelloController.java
@@ -1,7 +1,13 @@
 package br.com.ilabgrupo2.desafio.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import br.com.ilabgrupo2.desafio.utils.S3Util;
 
 @Controller
 public class HelloController {
@@ -11,27 +17,27 @@ public class HelloController {
     }
 	
      
-//    @PostMapping("/upload")
-//    public String handleUploadForm(Model model, String description,
-//            @RequestParam("file") MultipartFile multipart) {
-//        String fileName = multipart.getOriginalFilename();
-//         
-//        System.out.println("Description: " + description);
-//        System.out.println("filename: " + fileName);
-//         
-//        String message = "";
-//         
-//        try {
-//            UploadController.uploadFile(fileName, multipart.getInputStream());
-//            message = "Your file has been uploaded successfully!";
-//        } catch (Exception ex) {
-//            message = "Error uploading file: " + ex.getMessage();
-//        }
-//         
-//        model.addAttribute("message", message);
-//         
-//        return "message";              
-//    }
+    @PostMapping("/upload")
+    public String handleUploadForm(Model model, String description,
+            @RequestParam("file") MultipartFile multipart) {
+        String fileName = multipart.getOriginalFilename();
+         
+        System.out.println("Description: " + description);
+        System.out.println("filename: " + fileName);
+         
+        String message = "";
+         
+        try {
+            S3Util.uploadFile(fileName, multipart.getInputStream());
+            message = "Your file has been uploaded successfully!";
+        } catch (Exception ex) {
+            message = "Error uploading file: " + ex.getMessage();
+        }
+         
+        model.addAttribute("message", message);
+         
+        return "message";              
+    }
 }
 
 
--- src/main/java/br/com/ilabgrupo2/desafio/utils/S3Util.java
+++ src/main/java/br/com/ilabgrupo2/desafio/utils/S3Util.java
@@ -6,12 +6,16 @@ import java.io.InputStream;
 import software.amazon.awssdk.awscore.exception.AwsServiceException;
 import software.amazon.awssdk.core.exception.SdkClientException;
 import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.core.waiters.WaiterResponse;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.HeadObjectRequest;
+import software.amazon.awssdk.services.s3.model.HeadObjectResponse;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.waiters.S3Waiter;
 
 public class S3Util {
-	 private static final String BUCKET = "your-bucket-name";
+	 private static final String BUCKET = "grupo2-bucket";
      
 	    public static void uploadFile(String fileName, InputStream inputStream)
 	            throws S3Exception, AwsServiceException, SdkClientException, IOException {
@@ -24,5 +28,17 @@ public class S3Util {
 	         
 	        client.putObject(request,
 	                RequestBody.fromInputStream(inputStream, inputStream.available()));
+	        
+//	        S3Waiter waiter = client.waiter();
+//	        HeadObjectRequest waitRequest = HeadObjectRequest.builder()
+//	                            .bucket(BUCKET)
+//	                            .key(fileName)
+//	                            .build();
+//	         
+//	        WaiterResponse<HeadObjectResponse> waitResponse = waiter.waitUntilObjectExists(waitRequest);
+//	         
+//	        waitResponse.matched().response().ifPresent(x -> {
+//	            // run custom code that should be executed after the upload file exists
+//	        });
 	    }
 }
