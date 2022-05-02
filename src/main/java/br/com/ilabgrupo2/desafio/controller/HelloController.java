96cde7d17a1ec6dbcee8ca9a6e3bdd274e4e003d
lisdrl <lisandre.darioli@gmail.com>
2022-05-02 11:52:55 -0300

    Updating HelloController and home template

--- src/main/java/br/com/ilabgrupo2/desafio/controller/HelloController.java
+++ src/main/java/br/com/ilabgrupo2/desafio/controller/HelloController.java
@@ -1,15 +1,12 @@
 package br.com.ilabgrupo2.desafio.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
 	@GetMapping("/")
-    public String home(ModelMap model) {
-        model.addAttribute("nomeDoAtributo", "Treinaweb");
-
+    public String viewHomePage() {
         return "home";
     }
 }
