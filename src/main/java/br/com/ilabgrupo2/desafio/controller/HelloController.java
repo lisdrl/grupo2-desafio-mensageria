27f3511f72f921f8482a868a665c3f29ea878db2
Gustavo P. Nunes <gustavo-pradogt@hotmail.com>
2022-05-03 19:04:54 -0300

    fiz Controllerjava

--- src/main/java/br/com/ilabgrupo2/desafio/controller/HelloController.java
+++ src/main/java/br/com/ilabgrupo2/desafio/controller/HelloController.java
@@ -1,5 +1,6 @@
 package br.com.ilabgrupo2.desafio.controller;
 
+import br.com.ilabgrupo2.desafio.models.Produto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import br.com.ilabgrupo2.desafio.utils.S3Util;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HelloController {
@@ -19,27 +25,59 @@ public class HelloController {
      
     @PostMapping("/upload")
     public String handleUploadForm(Model model, String description,
-            @RequestParam("file") MultipartFile multipart) {
+            @RequestParam("file") MultipartFile multipart) throws IOException {
+        System.out.println(multipart);
         String fileName = multipart.getOriginalFilename();
-         
-        System.out.println("Description: " + description);
-        System.out.println("filename: " + fileName);
+//        System.out.println("filename: " + fileName);
+//        System.out.println("description" + description);
+        System.out.println(multipart);
+
+        BufferedReader br;
+        List<String> result = new ArrayList<>();
+        try {
+            String line;
+            InputStream is = multipart.getInputStream();
+            br = new BufferedReader(new InputStreamReader(is));
+            while ((line = br.readLine()) != null) {
+                String bre = "<p>" + line + "</p>";
+                result.add(bre);
+            }
+
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+
+        System.out.println(result);
+        String teste = result.toString().substring(1, result.toString().length()-1).replace(",", "").replace(";",  " | ");
+
          
         String message = "";
-        String variavel = "";
-         
+        String divClass = "";
+        String readedCsv = result.toString();
+//        String readedCsvHeader = readedCsv.substring(0, readedCsv.lastIndexOf("data")+4);
+//        String readedCsvContent = readedCsv.substring(readedCsv.lastIndexOf("data")+5, readedCsv.length()-1);
+//        System.out.println(readedCsvHeader);
+//        System.out.println(readedCsvContent);
+
         try {
             S3Util.uploadFile(fileName, multipart.getInputStream());
             message = "Upload realizado com sucesso!";
-            variavel = "umaclasse";
         } catch (Exception ex) {
             message = "Erro ao fazer upload: " + ex.getMessage();
-            variavel = "oier";
         }
          
         model.addAttribute("message", message);
-         
-        return "message";              
+        model.addAttribute("readedCsvHeader", teste);
+//        model.addAttribute("readedCsvContent", readedCsvContent);
+
+//        File f = new File();
+//        BufferedReader reader = new BufferedReader(multipart);
+//
+//        String line;
+//        Produto p = null;
+//        System.out.println(reader);
+
+        return "message";
     }
 }
 
