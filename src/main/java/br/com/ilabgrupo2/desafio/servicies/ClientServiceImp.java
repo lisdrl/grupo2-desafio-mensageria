5e31826b52b0ac5651ff48d20a04dc5a63a77858
Jean Pierre <tavares1988@live.com>
2022-05-11 09:09:07 -0300

    Criando service para cliente

---
+++ src/main/java/br/com/ilabgrupo2/desafio/servicies/ClientServiceImp.java
@@ -0,0 +1,27 @@
+package br.com.ilabgrupo2.desafio.servicies;
+
+import java.util.List;
+
+import br.com.ilabgrupo2.desafio.dto.CreateClientDTO;
+import br.com.ilabgrupo2.desafio.dto.ResponseClientDTO;
+
+public class ClientServiceImp implements IClientService {
+
+	@Override
+	public ResponseClientDTO createCliente(CreateClientDTO newClient) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseClientDTO getClientById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ResponseClientDTO> getAllClient() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ src/main/java/br/com/ilabgrupo2/desafio/servicies/IClientService.java
@@ -0,0 +1,12 @@
+package br.com.ilabgrupo2.desafio.servicies;
+
+import java.util.List;
+
+import br.com.ilabgrupo2.desafio.dto.CreateClientDTO;
+import br.com.ilabgrupo2.desafio.dto.ResponseClientDTO;
+
+public interface IClientService {
+	ResponseClientDTO createCliente(CreateClientDTO newClient);
+	ResponseClientDTO getClientById(Long id);
+	List<ResponseClientDTO> getAllClient();
+}
