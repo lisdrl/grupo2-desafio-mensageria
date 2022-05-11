4300f2919720644537bd93e484e88d6a0eb08a94
Jean Pierre <tavares1988@live.com>
2022-05-11 18:12:45 -0300

    Rota de cadastro de cliente funcionando 'caminho feliz'

--- src/main/java/br/com/ilabgrupo2/desafio/controller/ClientController.java
+++ src/main/java/br/com/ilabgrupo2/desafio/controller/ClientController.java
@@ -2,9 +2,10 @@ package br.com.ilabgrupo2.desafio.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.ilabgrupo2.desafio.dto.CreateClientDTO;
@@ -19,12 +20,22 @@ public class ClientController {
 	private IClientService clientService;
 	
 	 @GetMapping
-   public String viewClientePage() {
-     return "clientes";
+   public String clientForm(Model model) {
+		 model.addAttribute("cliente", new CreateClientDTO());
+     return "client/cliente";
    }
 	
 	@PostMapping
-	public ResponseClientDTO createClient (@RequestBody CreateClientDTO createClient) {
-		return clientService.createCliente(createClient);
+	public String createClient (@ModelAttribute CreateClientDTO cliente, Model model) {
+
+		ResponseClientDTO regClient = clientService.createCliente(cliente);
+		
+		if (regClient == null) {
+			model.addAttribute("error", "Erro ao cadastrar cliente!");
+			return "client/client_error";
+		}
+		
+		model.addAttribute("cliente", regClient);
+		return "client/client_result";
 	}
 }
