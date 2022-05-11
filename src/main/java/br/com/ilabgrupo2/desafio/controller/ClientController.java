0ccbe32b93f458cdc7d9871e5b1e6abf7e4d54fe
Jean Pierre <tavares1988@live.com>
2022-05-11 09:40:34 -0300

    Rota para cadastro de cliente

---
+++ src/main/java/br/com/ilabgrupo2/desafio/controller/ClientController.java
@@ -0,0 +1,24 @@
+package br.com.ilabgrupo2.desafio.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.ilabgrupo2.desafio.dto.CreateClientDTO;
+import br.com.ilabgrupo2.desafio.dto.ResponseClientDTO;
+import br.com.ilabgrupo2.desafio.servicies.IClientService;
+
+@Controller
+@RequestMapping("/clientes")
+public class ClientController {
+
+	@Autowired
+	private IClientService clientService;
+	
+	@PostMapping
+	public ResponseClientDTO createClient (@RequestBody CreateClientDTO createClient) {
+		return clientService.createCliente(createClient);
+	}
+}
