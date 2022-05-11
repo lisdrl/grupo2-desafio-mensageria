b0f8a12295856aa0065aa768fe1b171e97c1d08e
Jean Pierre <tavares1988@live.com>
2022-05-11 09:37:13 -0300

    Implementando serviço de cadastro para cliente

--- src/main/java/br/com/ilabgrupo2/desafio/servicies/ClientServiceImp.java
+++ src/main/java/br/com/ilabgrupo2/desafio/servicies/ClientServiceImp.java
@@ -2,15 +2,28 @@ package br.com.ilabgrupo2.desafio.servicies;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import br.com.ilabgrupo2.desafio.dto.CreateClientDTO;
 import br.com.ilabgrupo2.desafio.dto.ResponseClientDTO;
+import br.com.ilabgrupo2.desafio.model.Cliente;
+import br.com.ilabgrupo2.desafio.repository.ClientRepository;
 
 public class ClientServiceImp implements IClientService {
 
+	@Autowired
+	private ClientRepository clientRepository;
+	
 	@Override
 	public ResponseClientDTO createCliente(CreateClientDTO newClient) {
-		// TODO Auto-generated method stub
-		return null;
+		Cliente cliente = new Cliente();
+		cliente.setNome(newClient.getNome());
+		cliente.setTelefone(newClient.getTelefone());
+		
+		cliente = clientRepository.save(cliente);
+		
+		ResponseClientDTO clientResponse = new ResponseClientDTO(cliente);
+		return clientResponse;
 	}
 
 	@Override
