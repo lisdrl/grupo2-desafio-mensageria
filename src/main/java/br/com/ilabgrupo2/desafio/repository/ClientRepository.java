e6fe262f8875e7680f6c86c539963dea7c9c7643
Jean Pierre <tavares1988@live.com>
2022-05-13 11:15:15 -0300

    Adicionando query customizado

--- src/main/java/br/com/ilabgrupo2/desafio/repository/ClientRepository.java
+++ src/main/java/br/com/ilabgrupo2/desafio/repository/ClientRepository.java
@@ -1,7 +1,18 @@
 package br.com.ilabgrupo2.desafio.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import br.com.ilabgrupo2.desafio.dto.ResponseClientDTO;
 import br.com.ilabgrupo2.desafio.model.Cliente;
 
-public interface ClientRepository extends JpaRepository<Cliente, Long> {}
+public interface ClientRepository extends JpaRepository<Cliente, Long> {
+//	ResponseClientDTO findByTelefone(String telefone);
+	
+	@Query (" SELECT new "
+			+ "br.com.ilabgrupo2.desafio.dto.ResponseClientDTO(cliente) "
+			+ "FROM Cliente as cliente "
+			+ "WHERE cliente.telefone = :telefone")
+	public ResponseClientDTO findByTelefone(@Param("telefone") String telefone);
+}
